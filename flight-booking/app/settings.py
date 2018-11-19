20801fb6982fffc898eae60b0106e0296bdcf207
Fidelis Ojeah <fidelis_oj@yahoo.co.uk>
2018-11-19 20:54:37 +0100

    - include django test runner

--- flight-booking/app/settings.py
+++ flight-booking/app/settings.py
@@ -148,3 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+TEST_OUTPUT_VERBOSE = 2
+TEST_OUTPUT_DIR = 'test-results'
