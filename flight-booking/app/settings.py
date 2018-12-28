5e99bc9f03965f2dac8243676fd7ed8fb9eaf8a1
Fidelis Ojeah <fidelis_oj@yahoo.co.uk>
2018-12-28 12:18:04 +0100

    Update settings.py

--- flight-booking/app/settings.py
+++ flight-booking/app/settings.py
@@ -14,6 +14,8 @@ import os
 import sys
 import environ
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -213,3 +215,5 @@ JWT_AUTH = {
     'JWT_ALLOW_REFRESH': True
 
 }
+
+django_heroku.settings(locals())
