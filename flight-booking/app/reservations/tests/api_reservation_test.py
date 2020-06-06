eca5243d88e45353d6e62d67c59a2ebd1a728a8d
Fidelis Ojeah <fidelis.ojeah@gmail.com>
2020-06-06 19:42:40 -0400

    Update api_reservation_test.py

--- flight-booking/app/reservations/tests/api_reservation_test.py
+++ flight-booking/app/reservations/tests/api_reservation_test.py
@@ -537,9 +537,6 @@ class ReservationValid(ReservationTests):
         self.assertTrue(response.data.get('success'))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            len(payload.get('results')), 2
-        )
 
     def test_list_reservations_by_month(self):
         '''List/Filter Reservations by Month - Valid :- Payload correct'''
